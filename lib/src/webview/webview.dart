a25a0f6000f93e930778f018707bd4b52c51c5d0
TUSHAR BHAMBERE <tusharbhambere8@gmail.com>
2025-02-01 13:51:07 +0530

    Reove_Desktop_Supoort

--- lib/src/webview/webview.dart
+++ lib/src/webview/webview.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:abutil/abutil.dart';
 import 'webview_web.dart' if (dart.library.io) "webview_web_vain.dart";
-import 'webview_desktop.dart'
-    if (dart.library.html) "webview_desktop_vain.dart";
+// import 'webview_desktop.dart'
+//     if (dart.library.html) "webview_desktop_vain.dart";
 
 class Webview extends StatelessWidget {
   final String url;
@@ -14,9 +14,11 @@ class Webview extends StatelessWidget {
   Widget build(BuildContext context) {
     if (isWeb()) {
       return WebviewWeb(url: url);
-    } else if (isDesktop()) {
-      return WebviewDesktop(url: url);
-    } else {
+    }
+    // else if (isDesktop()) {
+    //   return WebviewDesktop(url: url);
+    // }
+    else {
       return const SizedBox.expand();
     }
   }
