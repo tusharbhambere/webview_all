b76c1212c7a4be273e8cd12b211f533a75759148
TUSHAR BHAMBERE <tusharbhambere8@gmail.com>
2025-02-01 12:42:48 +0530

    Only_Desktop_Support

--- lib/src/webview/webview.dart
+++ lib/src/webview/webview.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 import 'package:abutil/abutil.dart';
 import 'webview_web.dart' if (dart.library.io) "webview_web_vain.dart";
-import 'webview_mobile.dart';
 import 'webview_desktop.dart'
     if (dart.library.html) "webview_desktop_vain.dart";
 
@@ -14,12 +12,7 @@ class Webview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (isMobile()) {
-      return WebviewMobile(
-          url: url,
-          backgroundColor: const Color(0x00000000),
-          javaScriptMode: JavaScriptMode.unrestricted);
-    } else if (isWeb()) {
+    if (isWeb()) {
       return WebviewWeb(url: url);
     } else if (isDesktop()) {
       return WebviewDesktop(url: url);
